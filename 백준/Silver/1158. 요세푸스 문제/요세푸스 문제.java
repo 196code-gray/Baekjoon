48efc7ce4161efcb9cba286df0817368b1ad967c
gray <88307264+196code-gray@users.noreply.github.com>
2023-02-01 22:21:40 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 592 ms, Memory: 295952 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str[] = br.readLine().split(" ");
+		int N = Integer.valueOf(str[0]);
+		int K = Integer.valueOf(str[1]);
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 1; i <= N; i++) {
+			queue.offer(i);
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		while (queue.size() != 1) {
+			for (int i = 1; i < K; i++) {
+				queue.offer(queue.poll());
+			} // end of for
+			sb.append(queue.poll() + ", ");
+		}
+		sb.append(queue.poll() + ">");
+		System.out.println(sb);
+    }
+}
