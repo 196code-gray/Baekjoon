9d66a7399ff430f995b3777b96786eb71b06539d
gray <88307264+196code-gray@users.noreply.github.com>
2023-02-02 00:53:10 +0900

    [Silver V] Title: 돌 게임, Time: 220 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // n입력
+        int n = sc.nextInt();
+        // t의 숫자에 따라 조건문으로 아침, 점심 구분. s에 따라 밥알 구분. t의 숫자와 s의 숫가를 확인.
+        if (n % 2 == 0) {
+            System.out.println("CY");
+        }
+        else System.out.println("SK");
+    }
+}
