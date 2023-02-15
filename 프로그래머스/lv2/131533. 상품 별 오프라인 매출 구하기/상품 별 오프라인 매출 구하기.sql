bbce62bc649a62816a6a4a21f59cdafbb294400b
gray <88307264+196code-gray@users.noreply.github.com>
2023-02-15 21:04:02 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
+++ 프로그래머스/lv2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -4,8 +4,3 @@ join offline_sale on product.product_id = offline_sale.product_id
 group by product_code
 order by sales desc, product_code
 
-
-# SELECT PRODUCT_CODE,SUM(PRICE * SALES_AMOUNT) AS SALES FROM PRODUCT 
-# JOIN OFFLINE_SALE ON PRODUCT.PRODUCT_ID = OFFLINE_SALE.PRODUCT_ID
-# GROUP BY PRODUCT_CODE
-# ORDER BY SALES DESC,PRODUCT_CODE
