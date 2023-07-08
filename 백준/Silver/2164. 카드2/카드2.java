f7f250bf841a2dfc056b0d05e7c806e52de71f63
gray <88307264+196code-gray@users.noreply.github.com>
2023-07-08 18:05:40 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45468 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,23 +1,28 @@
+import java.io.*;
 import java.util.*;
-import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        // 카드 수량 받아옴
-        int card = s.nextInt();
-        //큐 생성
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 1; i <= card; i++) {
+
+        for (int i = 1; i <= n; i++) {
             queue.add(i);
         }
-        // 카드 하나 버리고 다음 카드 맨 뒤로 넘기기
-        // 카드 번호가 나와야 함.
 
+        int i = 1;
         while (queue.size() != 1) {
-            queue.poll();
-            queue.offer(queue.poll());
+            if (i % 2 != 0) {
+                queue.poll();
+                i++;
+            }
+            else {
+                queue.add(queue.poll());
+                i++;
+            }
         }
-        System.out.println(queue.peek());
+        System.out.println(queue.poll());
     }
 }
