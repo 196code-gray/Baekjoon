18f855885fda8bc79c001ba4300efcbb3568ecd3
gray <88307264+196code-gray@users.noreply.github.com>
2023-09-02 11:36:59 +0900

    [level 2] Title: 올바른 괄호, Time: 20.61 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        Stack<Character> stack = new Stack<>(); 
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') stack.push(s.charAt(i));
+            else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) return false;
+                else stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
