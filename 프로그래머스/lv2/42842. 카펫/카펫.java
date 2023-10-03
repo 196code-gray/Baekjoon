addd6cca83afa7934e98d6f0e6cee9cadc6583fc
gray <88307264+196code-gray@users.noreply.github.com>
2023-10-03 13:39:48 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int total = brown + yellow;
+        
+        for (int i = 3; i < total; i++){
+            if(total % i == 0){
+                int a = total / i;
+                int b = i;
+                
+                int row = Math.max(a, b);
+                int col = Math.min(a, b);
+                
+                if ((row - 2) * (col - 2) == yellow){
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
