ba2f12e9175b752a0afd8ad47af30c6e8c87c8c8
gray <88307264+196code-gray@users.noreply.github.com>
2023-11-25 13:17:39 +0900

    [Silver III] Title: 삼각형 만들기, Time: 1132 ms, Memory: 79808 KB -BaekjoonHub

--- 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
@@ -7,7 +7,7 @@ public class Main {
         public static void main(String[] args) throws Exception{
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int n = Integer.parseInt(br.readLine());
-            Integer[] arr = new Integer[n];
+            int[] arr = new int[n];
             for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(br.readLine());
             }
