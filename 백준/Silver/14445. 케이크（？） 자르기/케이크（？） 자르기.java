a6d77c4754ba5f68e4556bbf1c8862cdf3e1d774
gray <88307264+196code-gray@users.noreply.github.com>
2023-12-01 09:25:36 +0900

    [Silver I] Title: 케이크(?) 자르기, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/14445. 케이크（？） 자르기/케이크（？） 자르기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        if (n == 1) {
+            System.out.println(0);
+        } else {
+            System.out.println((n + 1) / 2);
+        }
+    }
+}
