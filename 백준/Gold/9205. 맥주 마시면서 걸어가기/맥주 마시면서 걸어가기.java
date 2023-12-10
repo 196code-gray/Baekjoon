62f81dcd8f8ed2f8f35c3b03cec8aa33ae1756f9
gray <88307264+196code-gray@users.noreply.github.com>
2023-12-10 16:19:03 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 156 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -2,56 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, sx, sy, ex, ey;
+    static int n, sx, sy, ex, ey;   // 편의점, 시작x, 시작y, 끝x, 끝y
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = null;
 
         int t = Integer.parseInt(br.readLine());
-        for(int tc=0; tc<t; tc++) {
-            n = Integer.parseInt(br.readLine());
-            List<int[]> list = new ArrayList<>();
-            for(int i=0; i<n+2; i++) {
-                st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < t; i++) {
+            n = Integer.parseInt(br.readLine());    // 편의점 개수
+            List<int[]> list = new ArrayList<>();  // 편의점 좌표 저장
+            for(int j = 0; j < n + 2; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                if(i==0) {
+                if(j == 0) {
                     sx = x;
                     sy = y;
-                }else if(i==n+1) {
+                } else if (j == n + 1) {
                     ex = x;
                     ey = y;
-                }else {
+                } else {
                     list.add(new int[]{x,y});
                 }
             }
 
-            bw.write(bfs(list)? "happy\n" : "sad\n");
+            if (bfs(list)) System.out.println("happy");
+            else System.out.println("sad");
         }
-
-        bw.flush();
-        bw.close();
     }
 
     static boolean bfs(List<int[]> list) {
         Queue<int[]> q = new LinkedList<>();
         boolean[] visited = new boolean[n];
-        q.add(new int[] {sx,sy});
+        q.add(new int[] {sx, sy});
+
         while(!q.isEmpty()) {
-            int[] pos = q.poll();
-            int px = pos[0], py = pos[1];
+            int[] store = q.poll();
+            int px = store[0];
+            int py = store[1];
             if(Math.abs(px- ex) + Math.abs(py- ey) <= 1000) {
                 return true;
             }
 
-            for(int i=0; i<n; i++) {
+            for(int i = 0; i < n; i++) {
                 if(!visited[i]) {
-                    int nx = list.get(i)[0], ny = list.get(i)[1];
+                    int nx = list.get(i)[0];
+                    int ny = list.get(i)[1];
                     int dis = Math.abs(px - nx) + Math.abs(py - ny);
                     if(dis <= 1000) {
                         visited[i] = true;
-                        q.add(new int[]{nx,ny});
+                        q.add(new int[] {nx, ny});
                     }
                 }
             }
