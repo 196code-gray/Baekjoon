70b5e7641665f912d27373e19e23ec68381f2827
gray <88307264+196code-gray@users.noreply.github.com>
2023-12-17 21:46:40 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.09 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,21 +1,31 @@
 import java.util.*;
 class Solution {
-    public long solution(int[] weights) {
-    	long answer = 0;
-        Arrays.sort(weights);
-        Map<Double, Integer> map = new HashMap<>();
-        for(int i : weights) {
-    		double a = i*1.0;
-    		double b = (i*2.0)/3.0;
-    		double c = (i*1.0)/2.0;
-    		double d = (i*3.0)/4.0;
-    		if(map.containsKey(a)) answer += map.get(a);
-    		if(map.containsKey(b)) answer += map.get(b);
-    		if(map.containsKey(c)) answer += map.get(c);
-    		if(map.containsKey(d)) answer += map.get(d);
-    		map.put((i*1.0), map.getOrDefault((i*1.0), 0)+1);
+   public long solution(int[] weights) {
+        long answer = 0;
+        long[] arr = new long[1001];
+
+        for(int i = 0; i < weights.length; i++){
+            arr[weights[i]]++;    
         }
-        
+
+        for(int i = 100; i < 1001; i++){
+            if(arr[i] == 0) continue;
+            answer += (arr[i] * (arr[i] - 1)) / 2;
+
+            if((4 * i) / 3 > 1000) continue;
+            if(i % 3 == 0){
+                answer += arr[i] * arr[(4 * i) / 3];
+            }
+
+            if((3 * i) / 2 > 1000) continue;
+            if(i % 2 == 0){
+                answer += arr[i] * arr[(3 * i) / 2];
+            }
+
+            if(2 * i >1000) continue;
+            answer += arr[i] * arr[2 * i];
+        }
+
         return answer;
     }
 }
