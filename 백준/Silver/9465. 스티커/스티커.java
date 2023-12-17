9aa27d1905a3310b12bb28c1d63093a8cd35d196
gray <88307264+196code-gray@users.noreply.github.com>
2023-12-17 10:18:27 +0900

    [Silver I] Title: 스티커, Time: 680 ms, Memory: 115376 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int Max (int a, int b) {
-        return a > b ? a : b;
-    }
+//    static int Max (int a, int b) {
+//        return a > b ? a : b;
+//    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -24,10 +24,10 @@ public class Main {
             dp[1][1] = arr[1][1];
 
             for (int i = 2; i <= n; i++) {
-                dp[0][i] = Max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
-                dp[1][i] = Max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
             }
-            System.out.println(Max(dp[0][n], dp[1][n]));
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
         }
         }
     }
