5d61cc634c5d1fdac78cd860fc5d1a219a06a303
gray <88307264+196code-gray@users.noreply.github.com>
2023-12-18 09:57:03 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 672 ms, Memory: 50504 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] meeting = new int[n][2];
+        int[][] meeting = new int[100_000][2];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,7 +13,7 @@ public class Main {
             meeting[i][1] = Integer.parseInt(st.nextToken());
         }
         PriorityQueue<Integer> q = new PriorityQueue<>();
-        Arrays.sort(meeting, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(meeting, Comparator.comparingInt(a -> a[0]));
 
         for (int[] i : meeting) {
             int s = i[0];
