c99ffdda4ef4295aa035989257eab03a79f3b66d
gray <88307264+196code-gray@users.noreply.github.com>
2023-12-21 19:46:14 +0900

    [Gold IV] Title: 테트로미노, Time: 532 ms, Memory: 30720 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,82 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int max = Integer.MIN_VALUE;
-	static int[][] arr;
-	static boolean[][] visit;
-	static int n;
-	static int m;
+    private static int N, M;
+    private static int[][] map;
+    private static int[][][] blockCase = new int[][][]{
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}}, // 네모 블럭
+            {{0, 0}, {0, 1}, {0, 2}, {0, 3}}, // l블록 가로배치
+            {{0, 0}, {1, 0}, {2, 0}, {3, 0}}, // l블록 세로 배치
+            {{0, 0}, {0, 1}, {0, 2}, {1, 1}}, // ㅜ블록 배치
+            {{0, 0}, {0, 1}, {0, 2}, {-1, 1}}, // ㅗ블록 배치
+            {{0, 0}, {1, 0}, {2, 0}, {1, 1}}, // ㅏ블록 배치
+            {{0, 0}, {1, 0}, {2, 0}, {1, -1}}, // ㅓ블록 배치
+            {{0, 0}, {1, 0}, {1, 1}, {2, 1}}, // 4번째 블록 정배치
+            {{0, 0}, {1, 0}, {1, -1}, {2, -1}}, // 4번째 블록 정배치 대칭
+            {{0, 0}, {0, -1}, {1, -1}, {1, -2}}, // 4번째 블록 우측 1회전
+            {{0, 0}, {0, 1}, {1, 1}, {1, 2}}, // 4번째 블록 우측 1회전 대칭
+            {{0, 0}, {1, 0}, {2, 0}, {2, 1}}, // L블록 배치
+            {{0, 0}, {1, 0}, {2, 0}, {2, -1}}, // L블록 대칭 배치
+            {{0, 0}, {1, 0}, {0, 1}, {0, 2}}, // ㄱ블록(가로 긴 버전) 대칭 배치
+            {{0, 0}, {0, 1}, {0, 2}, {1, 2}}, // ㄱ블록(가로 긴 버전) 배치
+            {{0, 0}, {0, 1}, {1, 1}, {2, 1}}, // ㄱ블룩(세로 긴 버전) 배치
+            {{0, 0}, {0, 1}, {1, 0}, {2, 0}}, // ㄱ블록(세로 긴 버전) 대칭 배치
+            {{0, 0}, {1, 0}, {1, 1}, {1, 2}}, // ㄴ블록 배치
+            {{1, 0}, {1, 1}, {1, 2}, {0, 2}} // ㄴ블록 대칭 배치
+    };
 
-	// 상하좌우
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        solve();
+    }
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    private static void solve() {
+        int ans = 0;
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new int[n][m];
-		visit = new boolean[n][m];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                for (int k = 0; k < blockCase.length; k++) {
+                    int sum = 0;
+                    boolean isBlock = true;
+                    for (int t = 0; t < 4; t++) {
+                        int bx = i + blockCase[k][t][0]; // 체크하고자 하는 블록의 x
+                        int by = j + blockCase[k][t][1]; // 체크하고자 하는 블록의 y
 
-		// 입력
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < m; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+                        if (bx < 0 || by < 0 || bx >= N || by >= M) {
+                            isBlock = false;
+                            break;
+                        }
 
-		// 전체 탐색 (dfs)
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				visit[i][j] = true;
-				solve(i,j,arr[i][j],1);
-				visit[i][j] = false;
-			}
-		}
+                        sum += map[bx][by];
+                    }
 
-		System.out.println(max);
-	}
+                    if (!isBlock) continue;
 
-	static void solve(int row, int col, int sum, int count) {
+                    ans = Math.max(ans, sum);
+                }
+            }
+        }
 
-		// 테트로미노 완성 시 수들의 합 계산
-		if(count == 4) {
-			max = Math.max(max, sum);
-			return;
-		}
-
-		// 상하좌우 탐색
-		for(int i = 0; i < 4; i++) {
-			int curRow = row + dx[i];
-			int curCol = col + dy[i];
-
-			// 범위 벗어나면 예외 처리
-			if(curRow < 0 || curRow >= n || curCol < 0 || curCol >= m) {
-				continue;
-			}
-
-			// 아직 방문하지 않은 곳이라면
-			if(!visit[curRow][curCol]) {
-
-				// 보라색(ㅗ) 테트로미노 만들기 위해 2번째 칸에서 탐색 한번 더 진행
-				if(count == 2) {
-					visit[curRow][curCol] = true;
-					solve(row, col, sum + arr[curRow][curCol], count + 1);
-					visit[curRow][curCol] = false;
-				}
-
-				visit[curRow][curCol] = true;
-				solve(curRow, curCol, sum + arr[curRow][curCol], count + 1);
-				visit[curRow][curCol] = false;
-			}
-		}
-	}
+        System.out.println(ans);
+    }
 }
