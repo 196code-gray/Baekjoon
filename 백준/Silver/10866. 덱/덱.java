fe68a5badff7ac2e2e7668da94e9d1125ff7bfef
gray <88307264+196code-gray@users.noreply.github.com>
2024-01-13 17:56:47 +0900

    [Silver IV] Title: 덱, Time: 200 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -4,6 +4,7 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n= Integer.parseInt(br.readLine());
         Deque<Integer> d = new ArrayDeque<>();
         while (n --> 0){
@@ -14,13 +15,14 @@ class Main{
                 if (s.equals("push_front")) d.offerFirst(m);
                 else d.offerLast(m);
             } else {
-                if (s.equals("pop_front")) System.out.println((d.isEmpty())?-1:d.pollFirst());
-                else if (s.equals("pop_back")) System.out.println((d.isEmpty())?-1:d.pollLast());
-                else if (s.equals("size")) System.out.println(d.size());
-                else if (s.equals("empty")) System.out.println((d.isEmpty())? 1 : 0);
-                else if (s.equals("front")) System.out.println((d.isEmpty())? -1 : d.peekFirst());
-                else if (s.equals("back")) System.out.println((d.isEmpty())? -1 : d.peekLast());
+                if (s.equals("pop_front")) sb.append((d.isEmpty())?-1:d.pollFirst()).append("\n");
+                else if (s.equals("pop_back")) sb.append((d.isEmpty())?-1:d.pollLast()).append("\n");
+                else if (s.equals("size")) sb.append(d.size()).append("\n");
+                else if (s.equals("empty")) sb.append((d.isEmpty())? 1 : 0).append("\n");
+                else if (s.equals("front")) sb.append((d.isEmpty())? -1 : d.peekFirst()).append("\n");
+                else if (s.equals("back")) sb.append((d.isEmpty())? -1 : d.peekLast()).append("\n");
             }
         }
+        System.out.println(sb);
     }
 }
