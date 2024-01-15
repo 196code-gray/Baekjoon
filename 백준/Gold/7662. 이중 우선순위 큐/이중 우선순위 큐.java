f15f63fa3eb1d037c4e8906f359d3cf6251682a9
gray <88307264+196code-gray@users.noreply.github.com>
2024-01-15 11:54:12 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2800 ms, Memory: 448540 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,32 +2,29 @@ import java.util.*;
 import java.io.*;
 
 class Main{
-    static TreeMap<Integer, Integer> map;
-    static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int t = Integer.parseInt(br.readLine());
         while (t --> 0){
             int k = Integer.parseInt(br.readLine());
-            map = new TreeMap<>();
-            sort(k);
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            while (k --> 0){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String s = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+                if (s.equals("I")) map.put(num, map.getOrDefault(num, 0) + 1);
+                else {
+                    if (map.isEmpty()) continue;
+
+                    num = (num == 1) ? map.lastKey() : map.firstKey();
+                    map.put(num, map.get(num) -1);
+                    if (map.get(num) == 0) map.remove(num);
+                }
+            }
+            sb.append((map.isEmpty()) ? "EMPTY" : map.lastKey() + " " + map.firstKey()).append("\n");
         }
         System.out.println(sb);
-    }
-    static void sort(int k) throws IOException {
-        while (k --> 0){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String s = st.nextToken();
-            int num = Integer.parseInt(st.nextToken());
-            if (s.equals("I")) map.put(num, map.getOrDefault(num, 0) + 1);
-            else {
-                if (map.isEmpty()) continue;
-
-                num = (num == 1) ? map.lastKey() : map.firstKey();
-                map.put(num, map.get(num) -1);
-                if (map.get(num) == 0) map.remove(num);
-            }
-        }
-        sb.append((map.isEmpty()) ? "EMPTY" : map.lastKey() + " " + map.firstKey()).append("\n");
     }
 }
