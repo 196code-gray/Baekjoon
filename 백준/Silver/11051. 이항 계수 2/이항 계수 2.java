19bbb44df9ea7d5c35565e4bd335333de5622b21
gray <88307264+196code-gray@users.noreply.github.com>
2024-01-16 21:07:17 +0900

    [Silver II] Title: 이항 계수 2, Time: 140 ms, Memory: 22132 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -1,24 +1,18 @@
-    import java.io.*;
-    import java.util.*;
+import java.util.*;
+import java.io.*;
 
-    public class Main {
-        public static void main(String args[]) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st= new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            if (k == 0){
-                System.out.println(1);
-                return;
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); int k = Integer.parseInt(st.nextToken());
+        long[][] dp = new long[n + 1][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i ++){
+            for (int j = 0; j <= i; j++){
+                if (j == 0) dp[i][0] = 1;
+                else dp[i][j] = (dp[i -1][j] + dp[i -1][j -1]) % 10007;
             }
-            long[][] dp = new long[1001][1001];
-            dp[0][0] = dp[1][0] = 1;
-            for (int i = 1; i <= n; i ++){
-                for (int j = 0; j <= i; j++){
-                    if (j == 0) dp[i][j] = 1;
-                    else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
-                }
-            }
-            System.out.println(dp[n][k] % 10007);
         }
-    }
+        System.out.println(dp[n][k] % 10007);
+    }}
