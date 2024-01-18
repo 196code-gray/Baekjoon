1ada20dcf2d73d2981cff19677ad7511e7bedc64
gray <88307264+196code-gray@users.noreply.github.com>
2024-01-18 12:20:33 +0900

    [Gold V] Title: 암호코드, Time: 188 ms, Memory: 16920 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s  = " " + br.readLine();
+        long[] dp = new long[s.length()];
+
+        if (s.charAt(1) == '0') {
+            System.out.println(0);
+            return;
+        }
+
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < s.length(); i++){
+            if ((s.charAt(i) -'0') > 0 && (s.charAt(i) -'0') < 10) dp[i] += (dp[i - 1]) % 1000000;
+            String c = String.valueOf(s.charAt(i -1)) + (s.charAt(i));
+            if ((Integer.parseInt(c)) > 9 && Integer.parseInt(c) < 27){
+                dp[i] += (dp[i -2]) % 1000000;
+            }
+//            else if (dp[i] == 0){
+//                dp[i] = dp[i -1];
+//            }
+        }
+        System.out.println(dp[s.length() -1] % 1000000);
+    }
+}
