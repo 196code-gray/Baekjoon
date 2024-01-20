1dd8be3501acbe60ef26652f4d6023c32bc51bb7
gray <88307264+196code-gray@users.noreply.github.com>
2024-01-20 18:07:38 +0900

    [Gold V] Title: 알약, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.java
+++ 백준/Gold/4811. 알약/알약.java
@@ -1,21 +1,22 @@
-    import java.io.*;
-    import java.util.*;
+import java.io.*;
+import java.util.*;
 
-    public class Main {
-        public static void main(String args[]) throws IOException {
-            long[][] dp = new long[31][31];
-            for (int h = 0; h <= 30; h++){
-                for (int w = h; w <= 30; w++){
-                    if (h == 0) dp[w][0] = 1;   // n개의 w개수는 항상 1개의 경우의 수가 존재
-                    else dp[w][h] = dp[w][h -1] + dp[w -1][h];
-                }
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            while (true){
-                int n = Integer.parseInt(br.readLine());
-                if (n == 0) break;
-                System.out.println(dp[n][n]);
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long[][] dp = new long[31][31];
+        for (int i = 0; i <= 30; i++){
+            for (int j = i;j <= 30; j++){
+                if (i == 0) dp[j][0] = 1;
+                else dp[j][i] = dp[j -1][i] + dp[j][i -1];
             }
         }
+        StringBuilder sb = new StringBuilder();
+        while (true){
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) break;
+            sb.append(dp[num][num]).append("\n");
+        }
+        System.out.println(sb);
     }
+}
