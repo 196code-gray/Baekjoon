bd0320b9d9b9cc6214eb1df3f808ab75592fd6a5
gray <88307264+196code-gray@users.noreply.github.com>
2024-01-28 08:08:13 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -1,33 +1,81 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 기적의 매매법
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int money =  Integer.parseInt(br.readLine());
-        int[] count = new int[2]; int[] result = new int[2];
-        int[] a = new int[14];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 14; i++) a[i] = Integer.parseInt(st.nextToken());
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
 
-        result[1] = result[0] = money;
-        for (int i = 0; i < 14; i++){
-            if(a[i] <= result[0]){
-                count[0] = (result[0] / a[i]);
-                result[0] -= (result[0] / a[i]) * a[i];
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        int money = Integer.parseInt(br.readLine());
+        int[] stock = new int[14];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 14; i++) {
+            stock[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        // 준현이
+        int money1 = money;
+        int cnt1 = 0;
+        for (int i = 0; i < 14; i++) {
+            cnt1 += money1 / stock[i];
+            money1 %= stock[i];
+        }
+
+
+        // 성민이
+        int money2 = money;
+        int cnt2 = 0;
+
+        for (int i = 3; i < 14; i++) {
+            // 3일 연속 가격이 상승
+            if (stock[i - 3] < stock[i - 2] && stock[i - 2] < stock[i - 1] && stock[i - 1] < stock[i]) {
+                money2 += (cnt2 * stock[i]);
+                cnt2 = 0;
+            }
+
+            // 3일 연속 가격이 하락
+            if (stock[i - 3] > stock[i - 2] && stock[i - 2] > stock[i - 1] && stock[i - 1] > stock[i]) {
+                cnt2 += (money2 / stock[i]);
+                money2 %= stock[i];
             }
         }
-        for (int i = 3; i < 14;i ++){
-            if (a[i -2] < a[i- 1] && a[i - 3] < a[i -2]) {
-                result[1] += a[i] * count[1];
-                count[1] = 0;
-            }
-            else if (a[i -1] < a[i -2] && a[i -2] < a[i -3]){
-                count[1] += result[1] / a[i];
-                result[1] -= (result[1]/a[i]) * a[i];
-            }
+
+        int result1 = money1 + (stock[13] * cnt1);
+        int result2 = money2 + (stock[13] * cnt2);
+        if (result1 > result2) {
+            System.out.println("BNP");
+        } else if (result1 == result2) {
+            System.out.println("SAMESAME");
+        } else {
+            System.out.println("TIMING");
         }
-        if ((result[0] + (a[13] * count[0])) == (result[1] + (a[13] * count[1]))) System.out.println("SAMESAME");
-        else System.out.println((result[0] + (a[13] * count[0])) > (result[1] + (a[13] * count[1])) ? "BNP" : "TIMING");
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
     }
 }
