6aa3b36be3061235b0aac58b13f3a6b0a6c43c41
gray <88307264+196code-gray@users.noreply.github.com>
2024-02-04 22:56:20 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2768 ms, Memory: 440840 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,26 +1,26 @@
 import java.util.*;
 import java.io.*;
 
-class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int t = Integer.parseInt(br.readLine());
-        while (t --> 0){
-            int k = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
             TreeMap<Integer, Integer> map = new TreeMap<>();
-            while (k --> 0){
+            for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String s = st.nextToken();
-                int num = Integer.parseInt(st.nextToken());
-                if (s.equals("I")) map.put(num, map.getOrDefault(num, 0) + 1);
-                else {
+                int now = Integer.parseInt(st.nextToken());
+                if (s.equals("I")) {
+                    map.put(now, map.getOrDefault(now, 0) + 1);
+                } else {
                     if (map.isEmpty()) continue;
 
-                    num = (num == 1) ? map.lastKey() : map.firstKey();
-                    map.put(num, map.get(num) -1);
-                    if (map.get(num) == 0) map.remove(num);
+                    int last = (now == 1) ? map.lastKey() : map.firstKey();
+                    map.put(last, map.get(last) -1);
+                    if (map.get(last) == 0) map.remove(last);
                 }
             }
             sb.append((map.isEmpty()) ? "EMPTY" : map.lastKey() + " " + map.firstKey()).append("\n");
