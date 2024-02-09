f4e59ece520d8c334009140c801aa8e1dadc8e07
gray <88307264+196code-gray@users.noreply.github.com>
2024-02-09 14:38:35 +0900

    [Silver II] Title: N과 M (9), Time: 216 ms, Memory: 39584 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n,m;
+    static int[] N;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
+        N = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            N[i] = Integer.parseInt(st.nextToken());
+        }
+        visited = new boolean[n];
+        Arrays.sort(N);
+        dfs(0, "");
+        System.out.println(sb);
+    }
+    static void dfs(int depth, String str){
+        if (depth == m){
+            if(!set.contains(str)){
+                set.add(str);
+                sb.append(str).append("\n");
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; i++){
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(depth + 1,  str + N[i] + " ");
+                visited[i] = false;
+            }
+        }
+    }
+}
