3dabd48271975a0351c001606792487956b82db5
gray <88307264+196code-gray@users.noreply.github.com>
2024-02-23 21:02:21 +0900

    [Gold V] Title: 치킨 배달, Time: 256 ms, Memory: 17884 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,71 +1,60 @@
-import java.lang.reflect.Array;
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main{
-    static int[][] map;
-    static boolean[] visited;
-    static List<Store> chicken;
-    static List<Home> home;
-    static int n, m, ans;
+public class Main {
+    static class Node {
+        int x, y;
 
-    public static void main(String[] args) throws IOException{
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    static int n, m, min = Integer.MAX_VALUE;
+    static boolean[] check;     // 해당 치킨집 방문 여부
+    static ArrayList<Node> home;    // 집 위치 저장
+    static ArrayList<Node> chicken; // 치킨집 위치 저장
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
+        n =Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
         home = new ArrayList<>(); chicken = new ArrayList<>();
-        for (int i = 0; i < n; i++){
+
+        for (int i = 0;i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j= 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1) home.add(new Home(i, j));
-                else if (map[i][j] == 2) chicken.add(new Store(i, j));
+            for (int j = 0; j < n; j++){
+                int now = Integer.parseInt(st.nextToken());
+                if (now == 1) home.add(new Node(i, j));
+                else if (now == 2) chicken.add(new Node(i, j));
             }
         }
-        ans = Integer.MAX_VALUE; visited = new boolean[chicken.size()];
-        distance(0,0);
-        System.out.println(ans);
-    }
 
-    private static void distance(int s, int e) {
-        if (e == m) {
-            int r = 0;
-            for (int i =0; i < home.size(); i++){
-                int count = Integer.MAX_VALUE;
+        check = new boolean[chicken.size()]; // 치킨 집 개수만큼
+        dfs(0,0);
+        System.out.println(min);
+    }
+    static void dfs(int count, int idx){
+        if (count == m){
+            int total = 0;
+            for (Node i : home) {
+                int sum = Integer.MAX_VALUE;
                 for (int j = 0; j < chicken.size(); j++){
-                    if (visited[j]) {
-                        int dis = Math.abs(home.get(i).x - chicken.get(j).x) + Math.abs(home.get(i).y - chicken.get(j).y);
-                        count = Math.min(count, dis);
+                    if (check[j]) {
+                    int dist = Math.abs(i.x - chicken.get(j).x) + Math.abs(i.y - chicken.get(j).y);
+                    sum = Math.min(sum, dist);
                     }
-                } r += count;
+                }
+                total += sum;
             }
-            ans = Math.min(ans, r);
-            return;
+            min = Math.min(min, total);
         }
-        for (int i = s; i < chicken.size(); i++){
-            visited[i] = true;
-            distance(i + 1, e + 1);
-            visited[i] = false;
-        }
-    }
-
-    static class Store{
-        int x;
-        int y;
-
-        public Store(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    static class Home {
-        int x;
-        int y;
-
-        public Home(int x, int y) {
-            this.x = x;
-            this.y = y;
+        for (int i = idx; i < chicken.size(); i++){
+            if (!check[i]){
+                check[i] = true;
+                dfs(count + 1, i + 1);
+                check[i] = false;
+            }
         }
     }
 }
