b518fd1b0126a6fe9b49e37c99187b17da2d2929
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-02 18:41:20 +0900

    [Gold V] Title: 동전 2, Time: 124 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -2,22 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
+    static int[] dj = {0,0,-1,1};
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        long[] dp = new long[100001];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        int n = Integer.parseInt(st.nextToken()); int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][k +1]; int[] coin = new int[n + 1];
+        int INF = 10_000_000;
 
-        for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            for (int j = num; j <= k; j++){
-                dp[j] = Math.min(dp[j], dp[j - num] + 1);
+        for (int i = 1; i<= n;i++){
+            coin[i] = Integer.parseInt(br.readLine());
+            Arrays.fill(dp[i], INF);
+        }
+        Arrays.fill(dp[0], INF);
+
+        for (int i = 1; i<= n; i++){
+            dp[i][0] = 1;
+            if(k >= coin[i]) dp[i][coin[i]] = 1;
+            for (int j = 1; j <= k; j++){
+                if (j - coin[i] >= 0){
+                    dp[i][j] = Math.min(dp[i][j - coin[i]] + 1, Math.min(dp[i -1][j], dp[i][j]));
+                    if (dp[i][j-coin[i]] !=INF) dp[i][j]=Math.min(dp[i][j],dp[i][j-1] + 1);
+                }
+                else dp[i][j] = dp[i -1][j];
             }
         }
 
-        System.out.println((dp[k] == Integer.MAX_VALUE) ? -1 : dp[k]);
+        System.out.println((dp[n][k] == INF) ? -1 : dp[n][k]);
     }
 }
