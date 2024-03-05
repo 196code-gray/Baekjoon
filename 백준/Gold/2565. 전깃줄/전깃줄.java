6df60fc92ce173e393b2f0f5e505a7f7c2b2e728
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-05 21:36:01 +0900

    [Gold V] Title: 전깃줄, Time: 80 ms, Memory: 11656 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i][0] = a;
+            arr[i][1]= b;
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0]; // 오름차순 정렬
+            }
+        });
+        int[] dp = new int[n];
+        int an = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) dp[i] = Math.max(dp[j]+1, dp[i]);
+            }
+        }
+        for (int i = 0; i < n; i++) an = Math.max(an, dp[i]);
+        System.out.println(n - an);
+    }
+}
