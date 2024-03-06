a6625c8f76edf613b2167686f79eeb66d67939f0
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-06 21:17:48 +0900

    [Silver I] Title: 물병, Time: 124 ms, Memory: 12908 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.java
+++ 백준/Silver/1052. 물병/물병.java
@@ -1,12 +1,12 @@
-import java.lang.reflect.Array;
 import java.util.*;
-import java.io.*;
 
 class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); int k = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int k = s.nextInt();
+
         int ans = 0;
         while (Integer.bitCount(n) > k){
             ans++;
