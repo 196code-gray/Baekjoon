4ea99621620703e0b3772a7ab693e6386500a434
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-12 21:04:11 +0900

    [Gold IV] Title: 빙산, Time: 1276 ms, Memory: 297112 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,100 +1,103 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main{
-    static int[] dx = {0, -1, 0 , 1};
-    static int[] dy = {-1, 0, 1, 0};
+public class Main {
     static int[][] map;
-    static boolean[][] visited;
-    static int n,m,time,count;
-    public static void main(String[] args) throws IOException {
+    static int n, m, time;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static boolean[][] visit;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         map = new int[n][m];
-        time = count = 0;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++){
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int now = 0;
-        while (true){
-            visited = new boolean[n][m];
-            if (iceberg() > 1) break;
-            if (count == 0) {
-                System.out.println(0);
-                return;
+        time = 0;
+        while (true) {
+            int p = piece();
+            if (p > 1) break; // 덩어리가 2개 이상일 경우 종료
+            if (p == 0) { // 얼음이 전부 녹았을 경우 종료
+                time = 0;
+                break;
             }
-            time = now;
-            visited = new boolean[n][m];
+            visit = new boolean[n][m];
 
-            for (int i = 0; i< n; i++){
-                for (int j = 0; j < m; j++){
-                    if (map[i][j] == 0 && !visited[i][j]){
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (!visit[i][j] && map[i][j] == 0) {
                         bfs(i, j);
                     }
                 }
             }
-            now ++;
+            time++;
         }
-        System.out.println(now);
+        System.out.println(time);
     }
 
-    private static void bfs(int x, int y) {
+    static int piece() {
+        int p = 0;
+        visit = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visit[i][j] && map[i][j] != 0) {
+                    p++;
+                    check(i, j);
+                }
+            }
+        }
+        return p;
+    }
+
+    static void bfs(int x, int y) {
         Queue<int[]> q = new LinkedList<>();
-        visited[x][y] = true;
         q.offer(new int[]{x, y});
-        while (!q.isEmpty()){
+        visit[x][y] = true;
+        while (!q.isEmpty()) {
             int[] now = q.poll();
-            for (int i = 0; i < 4; i++){
+            for (int i = 0; i < 4; i++) {
                 int nx = dx[i] + now[0];
                 int ny = dy[i] + now[1];
-                if (isBoundary(nx, ny) && !visited[nx][ny] && map[nx][ny] == 0){
+
+                if (nx >= n || ny >= m || nx < 0 || ny < 0 ) continue;
+                if (visit[nx][ny]) continue;
+
+                if (map[nx][ny] == 0) {
                     q.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                }
-                else if (isBoundary(nx, ny) && !visited[nx][ny] && map[nx][ny] == 1){
-                    map[nx][ny] -= 1;
-                    visited[nx][ny] = true;
-                } else if (isBoundary(nx, ny) && !visited[nx][ny] && map[nx][ny] != 0){
-                    map[nx][ny] -= 1;
-                }
+                    visit[nx][ny] = true;
+                } else if (map[nx][ny] == 1) {
+                    map[nx][ny] = 0;
+                    visit[nx][ny] = true;
+                } else map[nx][ny]--;
             }
         }
     }
-    static void findIceberg(int x, int y){
+
+    static void check(int x, int y) {
         Queue<int[]> q = new LinkedList<>();
-        visited[x][y] = true;
         q.offer(new int[]{x, y});
-        while (!q.isEmpty()){
+        visit[x][y] = true;
+        while (!q.isEmpty()) {
             int[] now = q.poll();
-            for (int i = 0; i < 4; i++){
+            for (int i = 0; i < 4; i++) {
                 int nx = dx[i] + now[0];
                 int ny = dy[i] + now[1];
-                if (isBoundary(nx, ny) && !visited[nx][ny] && map[nx][ny] != 0){
+
+                if (nx >= n || ny >= m || nx < 0 || ny < 0) continue;
+                if (visit[nx][ny]) continue;
+
+                if (map[nx][ny] != 0){
                     q.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
+                    visit[nx][ny] = true;
                 }
             }
         }
-    }
-
-    private static boolean isBoundary(int i, int j) {
-        return i >= 0 && j >= 0 && i < n && j < m;
-    }
-
-    private static int iceberg() {
-        count = 0;
-        for (int i = 0; i< n; i++){
-            for (int j = 0; j < m; j++){
-                if (map[i][j] != 0 && !visited[i][j]){
-                    count++;
-                    findIceberg(i, j);
-                }
-            }
-        }
-        return count;
     }
 }
