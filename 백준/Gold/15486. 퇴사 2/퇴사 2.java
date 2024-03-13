6b8d151ca4ca56b716b761d96b9f58be53669728
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-13 18:07:02 +0900

    [Gold V] Title: 퇴사 2, Time: 676 ms, Memory: 320040 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,22 +1,23 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] day = new int[n + 1][2];    // 상담 진행 기간, 금액
-        long[] dp = new long[1_500_002];    // 최댓값 150만 + 인덱스 1 + 퇴사일 + 1
-        for (int i = 1; i <= n; i ++){
+        int[] T,P; T = new int[n + 1]; P = new int[n + 1]; // 시간, 금액
+        long[] dp = new long[n + 2];
+        for (int i = 1; i<= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            day[i][0] = Integer.parseInt(st.nextToken());
-            day[i][1] = Integer.parseInt(st.nextToken());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i]  =Integer.parseInt(st.nextToken());
         }
+
         for (int i = n; i > 0; i--){
-            // 퇴사일을 넘어가는 경우
-            if (day[i][0] + i > n + 1) dp[i] = dp[i + 1];
-            // 퇴사일을 넘어가지 않는 경우
-            else dp[i] = Math.max(dp[i + 1], day[i][1] + dp[day[i][0] + i]);
+            if (T[i] + i <= n + 1){ // 기간 안에 가능
+                dp[i] = Math.max(dp[i + 1], dp[T[i] + i] + P[i]);
+            }
+            else dp[i] = dp[i + 1]; //기간 안에 불가능
         }
         System.out.println(dp[1]);
     }
