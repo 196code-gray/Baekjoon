7402d152aa40ed6e7b1f23a32ae29f7034a84c82
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-13 21:02:11 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1668 ms, Memory: 229052 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,59 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int[] num;
-	static boolean[][] dp;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-	
-		num = new int[n+1];
-		dp = new boolean[n+1][n+1];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1; i<n+1; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		for(int i = 1; i <= n; i++){
-			dp[i][i] = true;
-		}
-		
-		for(int i=1; i<=n-1; i++) {
-			if(num[i] == num[i+1]) dp[i][i+1] =true;
-		}
-		checkPalin(n);
-		
-		int t = Integer.parseInt(br.readLine());
-		for(int i=0; i<t; i++) {
-		 st = new StringTokenizer(br.readLine());
-		 int start = Integer.parseInt(st.nextToken());
-		 int end = Integer.parseInt(st.nextToken());
-		 
-		 if(dp[start][end]) {
-			sb.append("1\n");
-		 }
-		 else {
-			 sb.append("0\n");
-		 }
-		}
-		 
-		 System.out.println(sb);
-	}
-	
-	static void checkPalin(int n) {
-	
-		for(int i=2; i<n; i++) {
-			for(int j=1; j<=n-i; j++) {
-				if(num[j] == num[j+i] && dp[j+1][j+i-1]) {
-					dp[j][j+i] = true;
-				}
-			}
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (M --> 0){
+            st = new StringTokenizer(br.readLine());
+            int s= Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            boolean t = true;
+            while (s < e){
+                if (num[s++] != num[e--]){
+                    t = false;
+                    sb.append("0").append("\n");
+                    break;
+                }
+            }
+            if (t) sb.append("1").append("\n");
+        }
+        System.out.println(sb);
+    }
 }
