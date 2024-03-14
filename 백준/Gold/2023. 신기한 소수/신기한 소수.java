6134c5c2e4f865d8e6da1f651ea54eb6c7576c3e
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-14 16:08:18 +0900

    [Gold V] Title: 신기한 소수, Time: 484 ms, Memory: 11948 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,39 +1,33 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-    public class Main {
-        static int n;
-
-        public static void main(String[] args) throws Exception {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            n = Integer.parseInt(br.readLine());
-            dfs(2, 1);
-            dfs(3, 1);
-            dfs(5, 1);
-            dfs(7, 1);
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dfs(1, "2");
+        dfs(1, "3");
+        dfs(1, "5");
+        dfs(1, "7");
+        System.out.println(sb);
+    }
+    static void dfs(int depth, String ans){
+        if (depth == N){
+            sb.append(ans).append("\n");
+            return;
         }
-        static void dfs ( int num, int a) {
-            if (a == n) {
-                if (isPrime(num)) {
-                    System.out.println(num);
-                }
-                return;
-            }
-            for (int i = 1; i < 10; i++) {
-                if (i % 2 == 0) {
-                    continue;
-                }
-                if (isPrime(num * 10 + i)) {
-                    dfs(num * 10 + i, a + 1);
-                }
-            }
-        }
-        static boolean isPrime ( int numPri){
-            for (int i = 2; i <= numPri / 2; i++) {
-                if (numPri % i == 0) {
-                    return false;
-                }
-            }
-            return true;
+        for (int i = 1; i <= 9; i++){
+            int now = Integer.parseInt(ans + i);
+            if (isPrime(now)) continue;
+            dfs(depth + 1, String.valueOf(now));
         }
     }
+    static private boolean isPrime(int num){
+        for (int i = 2; i <= num/2; i++){
+            if (num % i == 0) return true;
+        }
+        return false;
+    }
+}
