19c83821278c33d2a48fb5880dc6921346f9070b
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-18 21:38:43 +0900

    [Gold IV] Title: 플로이드, Time: 456 ms, Memory: 81776 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,34 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); int m = Integer.parseInt(br.readLine());
-        int[][] price = new int[n][n];
-        for (int i = 0; i < n; i++){ // 각 위치 최댓값으로 지정
-            Arrays.fill(price[i], 100_000_000);
-            price[i][i] = 0; // 본인 위치 0
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] price = new int[n + 1][m + n];
+        final int INF = 10_000_000;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++){
+            Arrays.fill(price[i], INF);  // 최고값 저장
+            price[i][i] = 0; // 자기자신 초기화
         }
-        for (int k = 0; k < m; k++){
+        for (int i = 1;i <= m; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken()) -1; int e = Integer.parseInt(st.nextToken()) -1;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
             int p = Integer.parseInt(st.nextToken());
-            price[s][e] = Math.min(p, price[s][e]);
+            price[s][e] = Math.min(p, price[s][e]); // 비용 저장
         }
-        for (int k = 0; k < n; k++){
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++){
-                    if (price[i][j] > price[i][k] + price[k][j]){
-                        price[i][j] = price[i][k] + price[k][j];
-                    }
+        // 플로이드 와샬
+        for (int k = 1; k <= n; k++){  // 경유지
+            for (int i = 1; i <= n; i++){  // 출발지
+                for (int j = 1;j <= n; j++){  // 도착지
+                    if (k == i || k == j || i == j) continue;
+                    if (price[i][k] + price[k][j] > price[i][j]) continue;
+                    price[i][j] = price[i][k] + price[k][j];
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i= 0; i < n; i++){
-            for (int j = 0;j < n; j++){
-                if (price[i][j] == 100_000_000) sb.append(0).append(" ");
+        for (int i = 1;i <= n; i++){
+            for (int j= 1; j <= n; j++){
+                if (price[i][j] == INF) sb.append("0").append(" ");
                 else sb.append(price[i][j]).append(" ");
             }
             sb.append("\n");
