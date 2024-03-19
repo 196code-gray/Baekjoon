ac4a4d03477bedbdcc75106bd887a06a98816b94
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-19 22:34:37 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 96.36 ms, Memory: 208 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,38 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] board, int[][] skill) {
-        int answer = 0;
-        int[][] sum = new int[board.length + 1][board[0].length + 1];
-        for (int i = 0; i < skill.length; i++){
-            int type = skill[i][0];
+        int n = board.length;
+        int m = board[0].length;
+        int[][] sum = new int[n + 1][m + 1];
+        for (int i = 0; i < skill.length; i++) {
             int r1 = skill[i][1];
             int c1 = skill[i][2];
             int r2 = skill[i][3];
             int c2 = skill[i][4];
-            int num = skill[i][5];
-            sum[r1][c1] += ((type) == 1) ? -num : +num;
-            sum[r1][c2 + 1] += ((type) == 1) ? +num : -num;
-            sum[r2 + 1][c1] += ((type) == 1) ? +num : -num;
-            sum[r2 + 1][c2 + 1] += ((type) == 1) ? -num : +num;
+            int de = skill[i][5];
+            sum[r1][c1] += skill[i][0] == 1 ? -de : de;
+            sum[r1][c2 + 1] += skill[i][0] == 1 ? de : -de;
+            sum[r2 + 1][c1] += skill[i][0] == 1 ? de : -de;
+            sum[r2 + 1][c2 + 1] += skill[i][0] == 1 ? -de : de;
         }
-        arraySum(sum);
-        for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board[i].length; j++){
+        for (int j = 0; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                sum[i][j] += sum[i - 1][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                sum[i][j] += sum[i][j - 1];
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 board[i][j] += sum[i][j];
-                if (board[i][j] > 0) answer++;
+                ans = board[i][j] > 0 ? ans+=1 : ans;
             }
         }
-        return answer;
-    }
-    static void arraySum(int[][] sum){
-        for (int i = 0; i < sum[0].length -1; i++){
-            for (int j = 1; j < sum.length -1; j++){
-                sum[j][i] += sum[j -1][i];
-            }
-        }
-        for (int i = 0; i < sum.length -1; i++){
-            for (int j= 1; j < sum[0].length -1; j++){
-                sum[i][j] += sum[i][j -1];
-            }
-        }
+        return ans;
     }
 }
