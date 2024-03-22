31bc134b6ccf8240e0407f50214505bf0fd46e31
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-22 22:01:48 +0900

    [Gold V] Title: 배, Time: 292 ms, Memory: 15096 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,43 +1,43 @@
 import java.util.*;
 import java.io.*;
-class Main{
-    public static void main(String[] args) throws IOException {
+
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int time = 0;
-        int n= Integer.parseInt(br.readLine());
-        Integer[] a = new Integer[n];
+        int n = Integer.parseInt(br.readLine());
+        Integer[] crane = new Integer[n]; // 크레인
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++){
-            a[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(a, Collections.reverseOrder());
-        int m = Integer.parseInt(br.readLine());
-        List<Integer> box = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) crane[i] = Integer.parseInt(st.nextToken());
         int max = 0;
-        for (int i = 0; i< m; i++){
+
+        int m = Integer.parseInt(br.readLine());
+        List<Integer> box = new ArrayList<>(); // 박스
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
             box.add(Integer.parseInt(st.nextToken()));
-            if (max < box.get(i)) max = box.get(i);
+            max = Math.max(max, box.get(i));
         }
-        box.sort(Collections.reverseOrder());
-        int e = 0;
-        while (true){
-            if (a[0] < max) {
+
+        Arrays.sort(crane, Collections.reverseOrder());
+        Collections.sort(box, Collections.reverseOrder());
+        int time, e; time = e = 0;
+
+        while (true) {
+            if (max > crane[0]) {  // 박스 최고 무게를 크레인이 들 수 없는 경으
                 System.out.println(-1);
                 return;
             }
-            int boxidx = 0; int aidx = 0;
 
-            while (aidx < a.length){
-                if (boxidx == box.size()) break;
-                if (a[aidx] >= box.get(boxidx)) {
-                    box.remove(boxidx);
+            int craneIdx = 0;
+            int boxIdx = 0;
+            while (craneIdx < crane.length) {
+                if (boxIdx == box.size()) break; // 박스가 이제 없는 경우
+                if (crane[craneIdx] >= box.get(boxIdx)) {  // 현 크레인이 박스를 들 수 있는 경우
+                    box.remove(box.get(boxIdx));
                     e++;
-                    aidx++;
-                }
-                else boxidx++;
+                    craneIdx++;  // 다음 크레인
+                } else boxIdx++;
             }
-
             time++;
             if (e == m) break;
         }
