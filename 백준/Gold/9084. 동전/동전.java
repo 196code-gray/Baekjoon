a634c759a1e82238b4e9d1f3e193180c8fc297ed
gray <88307264+196code-gray@users.noreply.github.com>
2024-03-23 22:16:38 +0900

    [Gold V] Title: 동전, Time: 88 ms, Memory: 11784 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -1,32 +1,26 @@
-import java.util.*;
 import java.io.*;
+import java.util.StringTokenizer;
 
-class Main{
-    static int[] dp;
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         while (t --> 0){
             int n = Integer.parseInt(br.readLine());
-            int[] coin = new int[21];
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] coin = new int[n + 1];
+            for (int i = 1;i <= n; i++) coin[i] = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(br.readLine());
+            long[] dp = new long[k + 1];
+
+            dp[0]++;
             for (int i = 1; i <= n; i++){
-                coin[i] = Integer.parseInt(st.nextToken());
+               
+                for (int j = coin[i]; j <= k; j++){
+                    dp[j] += dp[j - coin[i]];
+                }
             }
-            dp = new int[10001];
-            dp[0] = 1;
-            int m = Integer.parseInt(br.readLine());
-            sb.append(dp(coin, m, n)).append("\n");
+            System.out.println(dp[k]);
         }
-        System.out.println(sb);
-    }
-    static int dp(int[] coin, int m, int n){
-        for (int i = 1; i <= n; i++){
-            for (int j = coin[i]; j <= m; j++){
-                dp[j] += dp[j - coin[i]];
-            }
-        }
-        return dp[m];
     }
 }
