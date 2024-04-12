00b44875ab823f1b668b99a1a7dd360aed60f5b2
gray <88307264+196code-gray@users.noreply.github.com>
2024-04-12 20:39:23 +0900

    [Gold V] Title: 알약, Time: 80 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.java
+++ 백준/Gold/4811. 알약/알약.java
@@ -6,7 +6,7 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long[][] dp = new long[31][31];
         for (int i = 0; i <= 30; i++){
-            for (int j = i;j <= 30; j++){
+            for (int j = i; j <= 30; j++){
                 if (i == 0) dp[j][0] = 1;
                 else dp[j][i] = dp[j -1][i] + dp[j][i -1];
             }
