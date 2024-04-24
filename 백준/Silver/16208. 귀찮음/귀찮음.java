3246b536bd3dc3c71ebb5659759478eadc45fce3
gray <88307264+196code-gray@users.noreply.github.com>
2024-04-24 17:37:39 +0900

    [Silver V] Title: 귀찮음, Time: 80 ms, Memory: 11516 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] stick = new int[n];
+        int len, ans; len = 0; ans = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n;i++) {
+            stick[i] = Integer.parseInt(st.nextToken());
+            len += stick[i];
+        }
+
+        for (int i = 0; i < n-1; i++){
+            len -= stick[i];
+            ans += len * stick[i];
+        }
+        System.out.println(ans);
+    }
+}
