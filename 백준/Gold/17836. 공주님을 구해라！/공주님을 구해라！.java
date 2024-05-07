3b504b92fc40ac35dd8584e11072c73f8e2d6a36
gray <88307264+196code-gray@users.noreply.github.com>
2024-05-07 15:28:53 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 92 ms, Memory: 12176 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -1,64 +1,86 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][][] visit;
-    static int[][] map;
-    static int n, m, t;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
-        t = Integer.parseInt(st.nextToken());
-
-        visit = new int[2][n][m]; map = new int[n][m];
-        for (int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                Arrays.fill(visit[0][i] ,10000);  // 임의의 큰 수
-                Arrays.fill(visit[1][i] ,10000);  // 임의의 큰 수
-            }
-        }
-        BFS();
-    }
-    static void BFS(){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0, 0, 0});
-        visit[0][0][0] = 0;
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            int gram = now[0]; int x = now[1]; int y = now[2];
-            int cnt = now[3];
-
-            if (cnt > t) continue;
-
-            for (int i = 0; i < 4; i++){
-                int nx= dx[i] + x;
-                int ny = dy[i] + y;
-
-                if (nx == n -1 && ny == m -1){
-                    System.out.println(cnt + 1);
-                    return;
-                }
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if (visit[gram][nx][ny] <= cnt + 1) continue;
-                if (gram == 1){
-                    q.offer(new int[]{gram, nx, ny, cnt + 1});
-                    visit[1][nx][ny] = cnt + 1;
-                } else {
-                    if (map[nx][ny] == 1) continue;
-                    if (map[nx][ny] == 2) { // 그람을 얻은 경우
-                        q.offer(new int[]{1, nx, ny, cnt+1});
-                    }
-                    else q.offer(new int[]{gram, nx, ny, cnt + 1});
-                    visit[0][nx][ny] = cnt + 1;
-                }
-            }
-        }
-        System.out.println("Fail");
-    }
+	static char[][] map;
+	static int N, M, T, dist, gram;
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
+		
+		map = new char[N][M];
+		dist = gram = Integer.MAX_VALUE;
+		
+		for(int r = 0; r < N; r++) {
+			String line = br.readLine();
+			for(int c = 0; c < M; c++) {
+				map[r][c] = line.charAt(2*c);
+			}
+		}
+		
+		bfs();
+		
+		int ans = dist == Integer.MAX_VALUE ? gram : dist;
+		
+		System.out.println(ans <= T ? ans : "Fail");
+	}
+	
+	static void bfs() {
+		Queue<Point> queue = new LinkedList<>();
+		boolean[][] visited = new boolean[N][M];
+		queue.add(new Point(0, 0, 0));
+		visited[0][0] = true;
+		
+		while(!queue.isEmpty()) {
+			Point p = queue.poll();
+			
+			if(p.r == N-1 && p.c == M-1) {
+				dist = p.t;
+				return;
+			}
+			
+			if(p.t > gram) return;
+			
+			for(int i = 0; i < 4; i++) {
+				int nr = p.r + dr[i];
+				int nc = p.c + dc[i];
+				
+				if(!isIn(nr, nc) || visited[nr][nc]) continue;
+				
+				if(map[nr][nc] == '0') {
+					queue.add(new Point(nr, nc, p.t+1));
+					visited[nr][nc] = true;
+				}
+				else if(map[nr][nc] == '2') {
+					visited[nr][nc] = true;
+					gram = p.t + 1 + (N-1-nr) + (M-1-nc);
+				}
+			}
+		}
+	}
+	
+	static boolean isIn(int r, int c) {
+		return 0 <= r && r < N && 0 <= c && c < M;
+	}
+	
+	static class Point {
+		int r, c, t;
+		
+		public Point(int r, int c, int t) {
+			this.r = r;
+			this.c = c;
+			this.t = t;
+		}
+	}
 }
