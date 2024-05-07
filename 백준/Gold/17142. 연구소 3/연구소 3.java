f1c5340db005b71df9d712d80982955d9afcabd0
gray <88307264+196code-gray@users.noreply.github.com>
2024-05-07 22:09:51 +0900

    [Gold III] Title: 연구소 3, Time: 216 ms, Memory: 43440 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -2,90 +2,90 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] map, time;
-    static boolean[][] visited;
-    static int n, m, ans = Integer.MAX_VALUE, empty, count;
-    static Queue<Point> q = new LinkedList<>();
+    static class Virus{
+        int x, y;
+
+        public Virus(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    static boolean[][] visit;
+    static int[][] map, v_arr;
+    static int n, m, blank, ans = Integer.MAX_VALUE;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static Point[] virus;
-    static List<Point> list = new ArrayList<>();
-
+    static ArrayList<Virus> v;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n= Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
-        map = new int[n][n]; time = new int[n][n]; empty = 0;
+        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
+        map = new int[n][n]; v_arr = new int[m][2]; v = new ArrayList<>();
+        blank = 0;
 
         for (int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) list.add(new Point(i, j));
-                if (map[i][j] == 0) empty++;
+                if (map[i][j] == 0) blank++;
+                if (map[i][j] == 2) v.add(new Virus(i ,j));
             }
         }
-        virus = new Point[m];
-        dfs(0, 0);
-        if (ans == Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(ans);
 
+        if (blank == 0){
+            System.out.println(0);
+            return;
+        }
+        DFS(0, 0);
+        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
     }
-
-    private static void dfs(int depth, int idx) {
-        if (depth == m){
-            visited = new boolean[n][n];
-            time = new int[n][n];
-            for (int i = 0; i < m; i++){
-                q.offer(new Point(virus[i].x, virus[i].y));
-                visited[virus[i].x][virus[i].y] = true;
-            }
-            bfs();
+    static void DFS(int depth, int idx){
+        if (depth == m) {
+            BFS();
             return;
         }
 
-        for (int i = idx; i < list.size(); i++){
-            virus[depth] = list.get(i);
-            dfs(depth + 1, i + 1);
-            visited[virus[depth].x][virus[depth].y] = false;
+        for (int i = idx; i < v.size(); i++){
+            int x = v.get(i).x;
+            int y = v.get(i).y;
+            v_arr[depth][0] = x;
+            v_arr[depth][1] = y;
+            DFS(depth + 1, i + 1);
+            visit[x][y]=false;
         }
     }
+    static void BFS(){
+        int max = 0; int c = 0;
+        Queue<int[]> q = new LinkedList<>();
+        visit = new boolean[n][n];
+        for (int i = 0; i < m; i++){
+            int x = v_arr[i][0];
+            int y = v_arr[i][1];
+            q.offer(new int[]{x, y, 0});
+            visit[x][y] = true;
+        }
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int x = now[0]; int y = now[1];
+            int cnt = now[2];
 
-    private static void bfs() {
-        count = 0; int result = 0;
+            for (int i = 0; i < 4; i++){
+                int nx= dx[i] + x;
+                int ny = dy[i] + y;
 
-        while (!q.isEmpty()){
-                Point now = q.poll();
-                int x = now.x;
-                int y = now.y;
-                for (int j = 0; j < 4; j++) {
-                    int nx = dx[j] + x;
-                    int ny = dy[j] + y;
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                if (visit[nx][ny] || map[nx][ny] == 1) continue;
 
-                if (isTure(nx, ny) && !visited[nx][ny] && map[nx][ny] != 1) {
-                    time[nx][ny] = time[x][y] + 1;
-                    if (map[nx][ny] == 0){
-                        count++;
-                        result = time[nx][ny];
-                    }
-                    q.offer(new Point(nx, ny));
-                    visited[nx][ny] = true;
+                q.offer(new int[]{nx, ny, cnt + 1});
+                visit[nx][ny] = true;
+                if (map[nx][ny] == 0) {
+                    c++;
+                    max = Math.max(cnt + 1, max);
                 }
-                }
+            }
         }
-        if (count == empty){
-            ans = Math.min(ans, result);
-        }
-    }
-    static boolean isTure (int x, int y){
-        return x >= 0 && y >= 0 && x < n && y < n;
-    }
-    static public class Point{
-        int x,y;
+        if (blank != c) return;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+        ans = Math.min(max, ans);
     }
 }
