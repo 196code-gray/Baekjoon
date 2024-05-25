dea17ae5729b127df8442c479a6c79d9801fb442
gray <88307264+196code-gray@users.noreply.github.com>
2024-05-25 21:10:15 +0900

    [Gold IV] Title: 뱀, Time: 84 ms, Memory: 12212 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,63 +1,70 @@
 import java.util.*;
 import java.io.*;
-class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int time = 0;
-        int n = Integer.parseInt(br.readLine());
-        int headx = 0; int heady = 0; int dir = 0;
-        int[][] map = new int[n][n];
-        int[] dx = {0, 1, 0,-1};
-        int[] dy = {1, 0, -1, 0};
-        Queue<int[]> q = new LinkedList<>();
-        int k = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < k; i++){
+class Main {
+    static BufferedReader br;
+    static int n, time, headx, heady, dir, k;
+    static Queue<int[]> q;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        time = 0;
+        n = Integer.parseInt(br.readLine());
+        headx = 0; heady = 0; dir = 0;
+        map = new int[n][n];
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        q = new ArrayDeque<>();
+        k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken()) -1;
-            int c = Integer.parseInt(st.nextToken()) -1;
-            map[r][c] = 1;
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            map[r][c] = 1; // 사과
         }
         int m = Integer.parseInt(br.readLine());
         q.offer(new int[]{0, 0});
         map[0][0] = -1;
-        while (m --> 0){
+        while (m --> 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            String c = st.nextToken();
-            while (time < x) {
+            int second = Integer.parseInt(st.nextToken()); // 초
+            String dist = st.nextToken(); // 방향
+            while (time < second) {
                 headx += dx[dir];
                 heady += dy[dir];
                 time++;
 
-                if (headx < 0 || heady < 0 || headx >= n || heady >= n) {
+                if (headx < 0 || heady < 0 || headx >= n || heady >= n) { // 벽에 부딪힐 경우
                     System.out.println(time);
                     return;
                 }
                 q.offer(new int[]{headx, heady});
-                if (map[headx][heady] == -1) {
+                if (map[headx][heady] == -1) { // 꼬리에 부딪힐 경우
                     System.out.println(time);
                     return;
                 }
-                if (map[headx][heady] == 1) {
+                if (map[headx][heady] == 1) { // 사과를 먹은 경우
                     map[headx][heady] = -1;
                     continue;
                 }
-                map[headx][heady] = -1;
+                map[headx][heady] = -1; // 꼬리 머리로 당겨오기
                 int[] now = q.poll();
-                map[now[0]][now[1]] = 0;
+                int x = now[0]; int y = now[1];
+                map[x][y] = 0;
             }
 
-            if(c.equals("L")){
+            if (dist.equals("L")) {
                 dir--;
                 if (dir == -1) dir = 3;
             }
-            if (c.equals("D")){
+            if (dist.equals("D")) {
                 dir++;
                 if (dir == 4) dir = 0;
             }
         }
-        while (true){
+        while (true) {
             headx += dx[dir];
             heady += dy[dir];
             time++;
@@ -66,7 +73,7 @@ class Main{
             q.offer(new int[]{headx, heady});
             if (map[headx][heady] == -1) break;
 
-            if (map[headx][heady] == 1){
+            if (map[headx][heady] == 1) {
                 map[headx][heady] = -1;
                 continue;
             }
