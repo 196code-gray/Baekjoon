247bc3ac674ba0b80fb9eab81dc98d33b8f051b9
gray <88307264+196code-gray@users.noreply.github.com>
2024-05-30 17:44:55 +0900

    [Silver II] Title: 배열 돌리기, Time: 944 ms, Memory: 172208 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
@@ -1,49 +1,73 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int[][] copy, a;
-    static int n;
+class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int[][] map, change;
+    static int n, t, d;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int  t= Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        br = new BufferedReader(new InputStreamReader(System.in));
+        t = Integer.parseInt(br.readLine());
+
         while (t --> 0){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken()); int turn = Integer.parseInt(st.nextToken());
-            a = new int[n][n]; copy = new int[n][n];
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken()); d = Integer.parseInt(st.nextToken());
+            map = new int[n][n]; change = new int[n][n];
+
             for (int i = 0; i < n; i++){
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < n; j++){
-                    a[i][j] = Integer.parseInt(st.nextToken());
-                    copy[i][j] = a[i][j];
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    change[i][j] = map[i][j];
                 }
             }
-            if (turn != 360) {
-                if (turn < 0) turn += 360;
-                turn /= 45;
 
-                while (turn-- > 0) turnArr();
-            }
+            if (Math.abs(d) == 360) {
+                save();
+            } else {
+                int now = d;
 
-            for (int i= 0;i < n; i++){
-                for (int j= 0; j < n; j++) {
-                    sb.append(a[i][j]).append(" ");
-                } sb.append("\n");
+                if (now < 0) now = 360 - (-d);
+
+                now /= 45;
+
+                while (now --> 0) {
+                    turn();
+                }
+
+                save();
             }
         }
         System.out.println(sb);
     }
-    static void turnArr(){
+
+    private static void save() {
         for (int i = 0; i < n; i++){
-            copy[i][n/2] = a[i][i];
-            copy[i][i] = a[n/2][i];
-            copy[n/2][i] = a[n - i -1][i];
-            copy[n -i -1][i] = a[n -i -1][n/2];
+            for (int j = 0; j < n; j++){
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+    }
+    static void turn(){
+        for (int i = 0; i < n / 2; i++){
+            change[i][n / 2] = map[i][i]; // 처음 왼대
+            change[i][n -1 -i] = map[i][n / 2];  // 위쪽 가운데
+            change[n/2][n -1 - i] = map[i][n -1 -i];  // 처음 오대
+            change[n -1 -i][n -1- i] = map[n / 2][n -1 -i];  // 중간 오
+            change[n - 1 - i][n / 2] = map[n - 1 - i][n -1- i]; // 아래 오대
+            change[n -1 -i][i] = map[n -1 -i][n / 2];  // 아래 중간
+            change[n / 2][i] =map[n -1-i][i]; // 아래 왼대
+            change[i][i] = map[n / 2][i];  //
         }
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++) a[i][j] = copy[i][j];
+        for (int i = 0; i < n ;i++){
+            for (int j = 0; j < n ;j++){
+                map[i][j] = change[i][j];
+            }
         }
     }
 }
