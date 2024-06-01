948326d541bb8cb0c2c1cb7e287b74e65460e5ea
gray <88307264+196code-gray@users.noreply.github.com>
2024-06-01 17:34:04 +0900

    [Gold IV] Title: 회사 문화 1, Time: 1284 ms, Memory: 67312 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -5,21 +5,18 @@ class Main {
     static BufferedReader br;
     static StringTokenizer st;
     static int n, m;
-    static ArrayList<Integer>[] list = new ArrayList[100_001];
-    static int[] total = new int[100_001];
+    static int[] total = new int[100_001], people = new int[100_001];
 
     public static void main(String[] args) throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i <= n; i++) list[i] = new ArrayList<>();
-
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i<=n;i++){
             int num = Integer.parseInt(st.nextToken());
             if (i == 1) continue;
-            list[num].add(i);
+            people[i] = num;
         }
 
         for (int i = 0;i < m; i++){
@@ -28,14 +25,11 @@ class Main {
             int score = Integer.parseInt(st.nextToken());
             total[s] += score;
         }
-        DFS(1);
+
+        for (int i = 1; i <= n; i++){
+            total[i] += total[people[i]];
+        }
 
         for (int i = 1; i <= n; i++) System.out.print(total[i] + " ");
-    }
-    static void DFS(int now){
-        for (Integer i : list[now]){
-            total[i] += total[now];
-            DFS(i);
-        }
     }
 }
