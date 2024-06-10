862e9bd5f15ad7989158454e494682542fa7947b
gray <88307264+196code-gray@users.noreply.github.com>
2024-06-10 18:34:51 +0900

    [Gold V] Title: 벽장문의 이동, Time: 72 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
@@ -1,60 +1,68 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] flow;
-    static boolean[] open = new boolean[21];
-    static int n,m,ans = Integer.MAX_VALUE;
+
+    static int n, open1, open2, m;
+    static int[] use;
+    // i번 이동했고, open1의 열린위치는 first, open2의 열린위치는 second 일때의 최소 이동횟수
+    static int[][][] dp;
+    static final int INF = Integer.MAX_VALUE;
+    static int ans;
+    static BufferedReader br;
+    static StringTokenizer st;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+         br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        open[Integer.parseInt(st.nextToken()) -1] = true; open[Integer.parseInt(st.nextToken()) -1] = true;
+        st = new StringTokenizer(br.readLine());
+        open1 = Integer.parseInt(st.nextToken()); open2 = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(br.readLine());
-        flow = new int[m];
-        for (int i = 0; i < m; i++){
-            flow[i] = Integer.parseInt(br.readLine())-1;
+
+        use = new int[m + 1];
+        for (int i = 1; i <= m; i++) {
+            use[i] = Integer.parseInt(br.readLine());
         }
 
-        dfs(0,0);
+        dp = new int[21][21][21]; // 현재 열어야하는 벽장, 모든 벽장, 모든 벽장
+
+        // dp배열 초기화
+        for (int i = 0; i <= m; i++) {
+            for (int first = 1; first <= n; first++) {
+                for (int second = 1; second <= n; second++) {
+                    dp[i][first][second] = INF;
+                }
+            }
+        }
+
+        dp[0][open1][open2] = 0; // 이동이 없는 경우(맨 처음)
+
+        ans = INF;
+        for (int i = 0; i < m; i++) {
+            for (int first = 1; first <= n; first++) {
+                for (int second = 1; second <= n; second++) {
+
+                    if (dp[i][first][second] == INF) continue; // 벽장문을 못 여는 경우
+
+                    // 첫번째 벽장을 옮기는 경우
+                    dp[i + 1][use[i + 1]][second] = 
+                            Math.min(dp[i + 1][use[i + 1]][second], dp[i][first][second] + Math.abs(first - use[i + 1]));
+                    
+                    // 두번째 벽장을 옮기는 경우
+                    dp[i + 1][first][use[i + 1]] = 
+                            Math.min(dp[i + 1][first][use[i + 1]], dp[i][first][second] + Math.abs(second - use[i + 1]));
+
+                    if (i == m - 1) {
+                        if (ans > dp[i + 1][use[i + 1]][second])
+                            ans = dp[i + 1][use[i + 1]][second];
+                        if (ans > dp[i + 1][first][use[i + 1]])
+                            ans = dp[i + 1][first][use[i + 1]];
+                    }
+                }
+            }
+        }
         System.out.println(ans);
-    }
-    static void dfs(int count, int now){
-        if (now == m){
-            ans = Math.min(ans, count);
-            return;
-        }
-        int move = flow[now];
-        if (open[move]) dfs(count, now + 1);
-        else {
-            int idx = -1;
-            for (int i = move; i < n; i++){
-                if (open[i]){
-                    idx = i;
-                    break;
-                }
-            }
-            if (idx != -1){
-                open[move] = true;
-                open[idx] = false;
-                dfs(idx - move + count , now + 1);
-                open[move] = false;
-                open[idx] = true;
-            }
-            idx = -1;
-            for (int i =  move; i >= 0; i--){
-                if (open[i]){
-                    idx = i;
-                    break;
-                }
-            }
-            if (idx != -1){
-                open[move] = true;
-                open[idx] = false;
-                dfs(move - idx + count, now + 1);
-                open[move] = false;
-                open[idx] = true;
-            }
-        }
     }
 }
