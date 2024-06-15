46f73118461586d24aecf8396c7b3d5d7a1f90ae
gray <88307264+196code-gray@users.noreply.github.com>
2024-06-15 18:42:54 +0900

    [Silver I] Title: 효율적인 해킹, Time: 3084 ms, Memory: 492940 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,55 +1,82 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main {
-    static int n, m;
-    static boolean[] visit;
-    static List<Integer>[] arr;
-    static int[] result;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        m = sc.nextInt();
-        arr = new List[n + 1];
-        result = new int[n + 1];
-        visit = new boolean[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            arr[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < m; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            arr[s].add(e);
-        }
-        for (int i = 1; i <= n; i++) {
-            visit = new boolean[n + 1];
-            BFS(i);
-        }
-        int big = 0;
-        for (int i = 1; i <= n; i++) {
-            big = Math.max(big, result[i]);
-        }
-        for (int i = 1; i <= n; i++) {
-            if (result[i] == big) {
-                System.out.print(i + " ");
-            }
-        }
-
-    }
-    public static void BFS(int n) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(n);
-        visit[n] = true;
-        while (!q.isEmpty()) {
-            int num = q.poll();
-            for (int i : arr[num]) {
-                if (visit[i] == false) {
-                    result[i]++;
-                    visit[i] = true;
-                    q.add(i);
-                }
-            }
-        }
-    }
+/**
+ * 완탐으로 풀면 시간초과난다.
+ * 시간복잡도를 우선 계산하고 완탐으로 할건지, dfs, bfs로 할건지 선택하자.
+ * 인접배열 형식으로 그래프를 표현하니 시간이 더 빠르다.
+ * @author quadcore
+ *
+ */
+public class Main{
+	public static int n,m;
+	public static boolean[][] map, visit;
+	public static int output = 0;
+	public static int max = 0;
+	public static ArrayList<Integer> hap;
+	public static boolean[] visited;
+	public static void main(String[] args) throws Exception, IOException {
+		//입력받고~
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		visit = new boolean[n][n];
+		visited = new boolean[n];
+		map = new boolean [n][n];
+		hap = new ArrayList<>();
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken())-1;
+			int y = Integer.parseInt(st.nextToken())-1;
+			map[y][x] = true;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			dfs(i);
+			//여기서 모아야
+			hap.add(output);
+			if (max < output) {
+				max = output;
+			}
+			output = 0;
+			visit[i] = visited;
+			visited = new boolean[n];
+		}
+//		System.out.println(hap);
+		int sibal = hap.size();
+		for (int i = 0; i < sibal; i++) {
+			if (hap.get(i) == max) {
+				System.out.print(i+1+" ");
+			}
+		}
+	}
+	
+	public static void dfs(int x) {
+		//방문했다는 표시 해주고.
+		visited[x] = true;
+		//가로로 보면서 해킹할 수 있는 컴퓨터를 보자.
+		for (int i = 0; i < map.length; i++) {
+			if (map[x][i] == false) continue;
+			if (visited[i] == true) continue;
+			if (hap.size() > i) {
+				int tmp = 0;
+				for (int j = 0; j < visit.length; j++) {
+					if (visit[i][j] == true && visited[j] == false) {
+						tmp = tmp + 1;
+						visited[j] = true;
+					}
+				}
+				output = output + tmp;
+			} else {
+				output = output + 1;
+				dfs(i);
+			}
+		}
+	}
 }
+
