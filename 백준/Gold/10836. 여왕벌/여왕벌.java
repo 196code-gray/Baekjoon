7c3b7c1bc312dbf25b4a70d9bb98b9baffa9e188
gray <88307264+196code-gray@users.noreply.github.com>
2024-06-19 19:07:28 +0900

    [Gold IV] Title: 여왕벌, Time: 888 ms, Memory: 123088 KB, Score: 83 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -1,64 +1,69 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Main {
-    static int[][] map, update;
-    static int[] input = new int[3];
-    static int n, m;
-    static BufferedReader br;
-    static StringTokenizer st;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
 
-    public static void main(String[] args) throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken()); 
+		int N = Integer.parseInt(st.nextToken()); 
 
-        map = new int[m][m];
-        update = new int[m][m];
-        for (int i = 0; i < m; i++)
-            Arrays.fill(map[i], 1);
+		int[][] size = new int[M][M];
 
-        while (n-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            input[0] = Integer.parseInt(st.nextToken());
-            input[1] = Integer.parseInt(st.nextToken());
-            input[2] = Integer.parseInt(st.nextToken());
-            int target = 0;
+		for(int i = 0; i < M; i++) 
+			Arrays.fill(size[i], 1);
 
-            for (int i = m - 1; i >= 0; i--) {
-                while(input[target] == 0) target++;
-                int now = target;
-                map[i][0] += now;
-                update[i][0] = now;
-                input[target]--;
-            }
+		for(int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine(), " "); 
+			int zero = Integer.parseInt(st.nextToken()); 
+			int one = Integer.parseInt(st.nextToken()); 
+			int two = Integer.parseInt(st.nextToken()); 
 
-            for (int i = 1; i < m; i++) {
-                if(input[target] == 0) target++;
-                int now = target;
-                map[0][i] += now;
-                update[0][i] = now;
-                input[target]--;
-            }
+			// 제일 왼쪽 열 애벌레 키우기 
+			for(int i = M-1; i > 0; i--) { 
+				if(zero != 0) {
+					zero--;
+				} else if(one != 0) {
+					one--;
+					size[i][0] += 1;
+				} else if(two != 0) {
+					two--;
+					size[i][0] += 2;
+				}
+			}
 
-            change();
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++)
-                sb.append(map[i][j]).append(" ");
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
+			// 제일 위쪽 행 애벌레 키우기 
+			for(int i = 0; i < M; i++) {
+				if(zero != 0) {
+					zero--;
+				} else if(one != 0) {
+					one--;
+					size[0][i] += 1;
+				} else if(two != 0) {
+					two--;
+					size[0][i] += 2;
+				}
+			}
+		}
 
-    static void change() {
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < m; j++) {
-                map[i][j] += Math.max(update[i][j - 1], Math.max(update[i - 1][j - 1], update[i - 1][j]));
-                update[i][j] = Math.max(update[i][j - 1], Math.max(update[i - 1][j - 1], update[i - 1][j]));
-            }
-        }
-    }
+		// 나머지 애벌레 키우기 
+		for(int i = 1; i < M; i++) {
+			for(int j = 1; j < M; j++)
+				size[i][j] = Math.max(size[i-1][j], Math.max(size[i-1][j-1], size[i][j-1]));
+		}
+		
+		// 출력
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) { 
+			for (int j = 0; j < M; j++) 
+				sb.append(size[i][j] + " "); 
+			sb.append("\n"); 
+		}
+
+		System.out.println(sb.toString());
+	}
 }
