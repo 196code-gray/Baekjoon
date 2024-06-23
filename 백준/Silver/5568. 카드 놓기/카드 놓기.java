4d1449d09ad85761a6f915bdb9b3e3ec719af816
gray <88307264+196code-gray@users.noreply.github.com>
2024-06-23 21:40:04 +0900

    [Silver IV] Title: 카드 놓기, Time: 92 ms, Memory: 13048 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.java
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.java
@@ -3,36 +3,34 @@ import java.util.*;
 
 public class Main {
     static int[] card;
-    static int n, k, ans;
-    static boolean[] vi;
-    static Set<Integer> set = new HashSet<>();
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static boolean[] visit;
+    static Set<String> set;
+    static BufferedReader br;
+    static int n, k;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         k = Integer.parseInt(br.readLine());
-        card = new int[n];
-        for (int i = 0; i < n; i++) card[i] = Integer.parseInt(br.readLine());
-        vi = new boolean[n];
 
-        dfs(0, "");
+        set = new HashSet<>();
+        card = new int[n]; visit = new boolean[n];
+        for (int i =0; i < n; i++)
+            card[i] = Integer.parseInt(br.readLine());
 
-        System.out.println(ans);
+        DFS(0, "");
+        System.out.println(set.size());
     }
-    static void dfs(int depth, String num){
+    static void DFS(int depth, String s){
         if (depth == k){
-            int f = Integer.parseInt(num);
-            if (!set.contains(f)){
-                ans++;
-                set.add(f);
-            }
+            set.add(s);
             return;
         }
-        for (int i = 0; i < n; i++){
-            if (!vi[i]) {
-                vi[i] = true;
-                dfs(depth + 1, num + card[i]);
-                vi[i]  = false;
-            }
+
+        for (int i= 0;i < card.length;i++){
+            if (visit[i]) continue;
+            visit[i] = true;
+            DFS(depth + 1, s + String.valueOf(card[i]));
+            visit[i] = false;
         }
     }
 }
