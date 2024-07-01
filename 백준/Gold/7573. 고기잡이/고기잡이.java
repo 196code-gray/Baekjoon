a70cd522814786233fe5c893034d0a68344b8dc1
gray <88307264+196code-gray@users.noreply.github.com>
2024-07-01 18:57:26 +0900

    [Gold III] Title: 고기잡이, Time: 236 ms, Memory: 12448 KB -BaekjoonHub

--- 백준/Gold/7573. 고기잡이/고기잡이.java
+++ 백준/Gold/7573. 고기잡이/고기잡이.java
@@ -22,7 +22,6 @@ public class Main{
         l = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        // 1,1 부터 시작
         for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
             int y = Integer.parseInt(st.nextToken());
@@ -32,7 +31,6 @@ public class Main{
 
         for(int i=0;i<M;i++){
             for(int j=0;j<M;j++){
-                // 물고기 중에 2개를 고름
                 catchFish(i,j);
             }
         }
@@ -40,7 +38,6 @@ public class Main{
         System.out.println(res);
     }
     static void catchFish(int fish1, int fish2){
-        // x,y값을 비교할 물고기 좌표 설정
         int y = fish.get(fish1).y;
         int x = fish.get(fish2).x;
 
@@ -49,25 +46,19 @@ public class Main{
         int h = (l - 2*v)/2;
 
         while(h>0){
-            // 잡을 수 있는 물고기 수
             int cnt = 0;
 
             for(int i=0;i<M;i++){
                 if(
                     // 두 물고기 중 한 물고기를 기준으로 그물 길이만큼의 x 길이 내에 물고기가 존재하는지 확인
-                        x <= fish.get(i).x &&
-                                fish.get(i).x <= x + h &&
+                        x <= fish.get(i).x && fish.get(i).x <= x + h &&
                                 // 두 물고기 중 한 물고기를 기준으로 그물 길이만큼의 y 길이 내에 물고기가 존재하는지 확인
-                                y <= fish.get(i).y &&
-                                fish.get(i).y <= y + v
+                                y <= fish.get(i).y && fish.get(i).y <= y + v
                 )
                     cnt++;
             }
-
-            // 현재 물고기 수와 답과의 최대값 구하기
             if(res < cnt) res = cnt;
 
-            // 다른 사이즈의 그물로 변환
             v++;
             h = (l - 2*v)/2;
         }
