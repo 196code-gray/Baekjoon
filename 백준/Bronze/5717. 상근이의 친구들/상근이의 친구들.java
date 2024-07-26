e33a3e5b68a30ca64673e8b81018fe25f4127108
gray <88307264+196code-gray@users.noreply.github.com>
2024-07-26 09:03:06 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 64 ms, Memory: 11316 KB -BaekjoonHub

--- 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -3,10 +3,11 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        InputReader in = new InputReader(System.in);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         while(true){
-            int m = in.readInt();
-            int f = in.readInt();
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int f = Integer.parseInt(st.nextToken());
 
             if (m == 0 && f == 0)
                 break;
@@ -14,67 +15,5 @@ public class Main {
             System.out.println(m + f);
         }
 
-    }
-    private static class InputReader {
-        private InputStream stream;
-        private byte[] buf = new byte[1024];
-        private int curChar;
-        private int numChars;
-        private SpaceCharFilter filter;
-
-        public InputReader(InputStream stream) {
-            this.stream = stream;
-        }
-
-        public int read() {
-            if (numChars == -1) {
-                throw new InputMismatchException();
-            }
-            if (curChar >= numChars) {
-                curChar = 0;
-                try {
-                    numChars = stream.read(buf);
-                } catch (IOException e) {
-                    throw new InputMismatchException();
-                }
-                if (numChars <= 0) {
-                    return -1;
-                }
-            }
-            return buf[curChar++];
-        }
-
-        public int readInt() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            int res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        public boolean isSpaceChar(int c) {
-            if (filter != null) {
-                return filter.isSpaceChar(c);
-            }
-            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-        }
-
-        public interface SpaceCharFilter {
-            public boolean isSpaceChar(int ch);
-        }
     }
 }
