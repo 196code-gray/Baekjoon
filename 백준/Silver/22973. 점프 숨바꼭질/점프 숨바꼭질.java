1ae617c80c3b3bb3867e1fe5241d65ce49f619cf
gray <88307264+196code-gray@users.noreply.github.com>
2024-08-27 08:04:39 +0900

    [Silver II] Title: 점프 숨바꼭질, Time: 64 ms, Memory: 11504 KB, Score: 125 point -BaekjoonHub

--- 백준/Silver/22973. 점프 숨바꼭질/점프 숨바꼭질.java
+++ 백준/Silver/22973. 점프 숨바꼭질/점프 숨바꼭질.java
@@ -2,17 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long K = Long.parseLong(br.readLine());
-        
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                long K = Math.abs(Long.parseLong(br.readLine()));
+
         if (K == 0) {
             System.out.println(0);
         } else if (K % 2 == 0) {
             System.out.println(-1);
         } else {
-            System.out.println(Integer.toBinaryString((int)K).length());
+            int ans = 0;
+            while (K > 0) {
+                K = K >> 1;
+                ans++;
+            }
+            System.out.println(ans);
         }
-        
-    }
+            }
 }
