729fcbda4a959f9aa48d43b9df7d7bde61b22e46
gray <88307264+196code-gray@users.noreply.github.com>
2024-10-08 07:54:27 +0900

    [Gold V] Title: 레벨 햄버거, Time: 88 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Gold/16974. 레벨 햄버거/레벨 햄버거.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+	static int N;
+	static long X;
+	static long[] total, pat;
+	static int cnt = 0;
+	
+    public static void main(String[] args) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	N = sc.nextInt();
+    	X = sc.nextLong();
+    	
+    	sc.close();
+    	
+    	total = new long[N+1]; 
+    	pat = new long[N+1];
+    	total[0] = 1;
+    	pat[0] = 1; 
+
+    	for(int i=1; i<=N; i++) {
+    		total[i] = 1 + total[i-1] + 1 + total[i-1] + 1;
+    		pat[i] = pat[i-1] + 1 + pat[i-1];
+    	}
+
+    	long ans = recursion(N, X);
+    	
+    	System.out.println(ans);
+    }
+
+    static long recursion(int n, long x) {
+    	if(n == 0) {
+    		if(x == 0) {
+    			return 0;
+    		} 
+    		else if(x == 1) {
+    			return 1;
+    		}
+    	}
+
+    	if(x == 1) {
+    		return 0;
+    	} 
+    	else if(x <= 1 + total[n-1]) {
+    		return recursion(n-1, x-1);
+    	} 
+    	else if(x == 1 + total[n-1] + 1) {
+    		return pat[n-1] + 1;
+    	} 
+    	else if(x <= 1 + total[n-1] + 1 + total[n-1]) {
+    		return pat[n-1] + 1 + recursion(n-1, x - (1 + total[n-1] + 1));
+    	} 
+    	else {
+    		return pat[n-1] + 1 + pat[n-1];
+    	}
+    }
+}
